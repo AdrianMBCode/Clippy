86c4402ca050a718537912cd41c60673ea797bf7
Rocio Salgado <salgadorocio@hotmail.com>
2023-02-22 18:30:05 +0100

    adds views

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,12 +6,13 @@ const Ticket = require("../models/Ticket.model")
 
 
 router.get("/:id/tickets/:ticketId", (req,res,next)=>{
-    let id = req.params.id
-    let ticketId = req.params.ticketId
-    Ticket.findById(ticketId)
-    .then((result)=>{
-        res.render("senior/edit-ticket", {ticket: result, id});
-    })
+    // let id = req.params.id
+    // let ticketId = req.params.ticketId
+    // Ticket.findById(ticketId)
+    // .then((result)=>{
+        // ***********************agregar {ticket: result, id} dp de render****************************
+        res.render("senior/edit-ticket");
+    // })
 })
 
 router.post("/:id/tickets/:ticketId", (req, res, next) => {
@@ -27,25 +28,26 @@ router.post("/:id/tickets/:ticketId", (req, res, next) => {
 });
 
 router.get("/:id/solved", (req, res, next) => {
-    let id = req.params.id
-    console.log(id)
-    User.findById(id)
-    .then((result)=>{
-        if(result.role === "senior"){
-            Ticket.find({$and:[{'solution.author': id}, {'isPending': false}]})
-            .then((result)=>{
-                res.render("senior/solved-tickets", {tickets:result});
-            })
-            .catch((err)=>console.log(err))
-        }else if (result.role === "junior"){
-            Ticket.find({$and:[{'author': id}, {'isPending': false}]})
-            .then((result)=>{
-                res.render("junior/solved-tickets", {tickets:result});
-            })
-        }else{
-            res.redirect("/auth/login")
-        }
-    }).catch((err)=>console.log(err))
+    // let id = req.params.id
+    // console.log(id)
+    // User.findById(id)
+    // .then((result)=>{
+    //     if(result.role === "senior"){
+    //         Ticket.find({$and:[{'solution.author': id}, {'isPending': false}]})
+    //         .then((result)=>{
+                // res.render("senior/solved-tickets", result);
+    //         })
+    //         .catch((err)=>console.log(err))
+    //     }else if (result.role === "junior"){
+    //         Ticket.find({$and:[{'author': id}, {'isPending': false}]})
+    //         .then((result)=>{
+        // ****************************agregar {tickets:result} dp render************************************
+                res.render("junior/solved-tickets");
+    //         })
+    //     }else{
+    //         res.redirect("/auth/login")
+    //     }
+    // }).catch((err)=>console.log(err))
 });
 
 router.post("/:id/tickets", (req, res, next) => {
@@ -60,47 +62,50 @@ router.post("/:id/tickets", (req, res, next) => {
 });
 
 router.get("/:id/tickets", (req, res, next) => {
-    let id = req.params.id
-    User.findById(id)
-    .then((result)=>{
-        if(result.role === "senior"){
-            Ticket.find()
-            .then((result)=>{
-                res.render("senior/tickets", {tickets:result});
-            })
-            .catch((err)=>console.log(err))
-        }
-        else if (result.role === "junior"){
-           res.render("junior/create-ticket", result);
-        }
-        else{res.redirect("/auth/login")}
-    }).catch((err)=>console.log(err))
+    // let id = req.params.id
+    // User.findById(id)
+    // .then((result)=>{
+    //     if(result.role === "senior"){
+    //         Ticket.find()
+    //         .then((result)=>{
+    //             res.render("senior/tickets", {tickets:result});
+    //         })
+    //         .catch((err)=>console.log(err))
+    //     }
+    //     else if (result.role === "junior"){
+            // ********************************agrergar result dp render*********************************
+           res.render("junior/create-ticket");
+    //     }
+    //     else{res.redirect("/auth/login")}
+    // }).catch((err)=>console.log(err))
 });
 
 router.get("/:id/pending", (req,res,next)=>{
-    let id = req.params.id
-    Ticket.find({$and:[{'author': id}, {'isPending': true}]})
-    .then((result)=>{
-        res.render("junior/pending-tickets", result);
-    })
-    .catch((err)=>console.log(err))
+    // let id = req.params.id
+    // Ticket.find({$and:[{'author': id}, {'isPending': true}]})
+    // .then((result)=>{
+        // ************************agregar result dp render****************************
+        res.render("junior/pending-tickets");
+    // })
+    // .catch((err)=>console.log(err))
 })
 
 router.get("/:id", (req, res, next) => {
-    let id = req.params.id
-    User.findById(id)
-    .then((result)=>{
-        if(result.role === "senior"){
-            res.render("senior/profile", result)
-        }else if (result.role === "junior"){
-            res.render("junior/profile", result)
-        }else if(result.role === "admin"){
-            res.render("admin/profile", result)
-        }else{
-            res.redirect("/auth/login")
-        }
-    })
-    .catch((err)=>console.log(err))
+    // let id = req.params.id
+    // User.findById(id)
+    // .then((result)=>{
+    //     if(result.role === "senior"){
+        // ***************agregar result dp render****************
+            res.render("senior/profile")
+    //     }else if (result.role === "junior"){
+    //         res.render("junior/profile", result)
+    //     }else if(result.role === "admin"){
+    //         res.render("admin/profile", result)
+    //     }else{
+    //         res.redirect("/auth/login")
+    //     }
+    // })
+    // .catch((err)=>console.log(err))
 });
 
 
